aac9f2eeccbecf4f7563bcb9a7a6833bdf4cc1ff
Sravan2808 <141347235+Sravan2808@users.noreply.github.com>
2024-03-31 17:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (99.69%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,22 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-
-        List<Integer> curr=new ArrayList<>();
-        result.add(curr);
-
-        for(int num:nums){
-            int n=result.size();
-            for(int j=0;j<n;j++){//copying the previous subset from previous iteration
-                List<Integer> temp=new ArrayList<>(result.get(j));
-                temp.add(num);//adding current number in all the previous subsets
-                result.add(temp);//add it,back to the result
-            }
+    public void backtrack(int idx,List<List<Integer>> result,ArrayList<Integer> currList,int nums[])
+    {
+        result.add(new ArrayList(currList));
+        for(int i=idx;i<nums.length;i++)
+        {
+            //added current element in the result
+            currList.add(nums[i]);
+            // generate further
+            backtrack(i+1,result,currList,nums);
+            // once all exploration remove the last element
+            currList.remove(currList.size()-1);
         }
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> currList = new ArrayList<>();
+        backtrack(0,result,currList,nums);
         return result;
+        
     }
 }
