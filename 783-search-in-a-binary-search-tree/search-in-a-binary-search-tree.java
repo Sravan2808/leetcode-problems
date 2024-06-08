01e2f414f27f61a91b1dd801c2c510e4db583861
Sravan2808 <141347235+Sravan2808@users.noreply.github.com>
2024-06-08 10:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (59.31%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -13,20 +13,40 @@
  *     }
  * }
  */
+//  Time complexity : O(h)
+// Space complexity : O(h)
+
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        // base case condition
-        if(root == null || root.val==val){
-            return root;
+        // Recursive approach
+        // // base case condition
+        // if(root == null || root.val==val){
+        //     return root;
+        // }
+        // // traverse the left subtree
+        // if(val < root.val){
+        //     return searchBST(root.left,val);
+        // }
+        // else{
+        //     // traverse the right subtree
+        //     return searchBST(root.right,val);
+        // }
+
+        // Iterative approach
+
+        TreeNode current=root;
+        TreeNode parentNode=null;
+        while(current!=null && current.val!=val){
+            if(val<current.val){
+                // traverse towards left side
+                current=current.left;
+            }
+            else{
+                // traverse towards right side
+                current=current.right;
+            }
         }
-        // traverse the left subtree
-        if(val < root.val){
-            return searchBST(root.left,val);
-        }
-        else{
-            // traverse the right subtree
-            return searchBST(root.right,val);
-        }
+        return current;
         
     }
 }
