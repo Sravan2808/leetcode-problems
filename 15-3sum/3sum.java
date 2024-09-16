936467756cb9f9b5f4f93933a950b929f98ba1e9
Sravan2808 <141347235+Sravan2808@users.noreply.github.com>
2024-09-16 09:58:23 +0530

    Time: 29 ms (71.61%) | Memory: 51.1 MB (94.20%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -16,21 +16,51 @@ class Solution {
         // List<List<Integer>> ans = new ArrayList<>(st);
         // return ans;
 
-        // Brute Force Approach
-        Set<List<Integer>> st = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            Set<Integer> hashset = new HashSet<>();
-            for(int j=i+1;j<nums.length;j++){
-                int third = -(nums[i]+nums[j]);
-                if(hashset.contains(third)){
-                    List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
-                    temp.sort(null);
-                    st.add(temp);
+        // Brute Force Approach TC:O(n^2) SC:O(n)
+        // Set<List<Integer>> st = new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     Set<Integer> hashset = new HashSet<>();
+        //     for(int j=i+1;j<nums.length;j++){
+        //         int third = -(nums[i]+nums[j]);
+        //         if(hashset.contains(third)){
+        //             List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
+        //             temp.sort(null);
+        //             st.add(temp);
+        //         }
+        //         hashset.add(nums[j]);
+        //     }
+        // }
+        // List<List<Integer>> ans = new ArrayList<>(st);
+        // return ans;
+
+        // Optimized Approach
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+
                 }
-                hashset.add(nums[j]);
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+
+                }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
         
     }
