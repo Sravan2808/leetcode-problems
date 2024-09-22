a6b918d97312dc7753bce2a54abb4527b484f57b
Sravan2808 <141347235+Sravan2808@users.noreply.github.com>
2024-09-22 20:14:11 +0530

    Time: 2 ms (96.60%) | Memory: 45.5 MB (56.76%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -15,18 +15,37 @@ class Solution {
         // int ans[] ={repeating,missing};
         // return ans;
 
-        // BetterApproach(Hashing)  
-        int repeating=-1,missing=-1;
-        int n = nums.length;
-        int[] hash = new int[n+1];
+        // BetterApproach(Hashing)  TC:O(n) Sc:O(n)
+        // int repeating=-1,missing=-1;
+        // int n = nums.length;
+        // int[] hash = new int[n+1];
+        // for(int i=0;i<n;i++){
+        //     hash[nums[i]]++;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     if(hash[i]==2) repeating=i;
+        //     else if(hash[i]==0) missing=i;
+        // }
+        // int ans[]={repeating,missing};
+        // return ans;
+
+        // OptimalApproach 
+        long n=nums.length;
+        // S-Sn
+        // S2-S2n
+        long Sn = (n*(n+1))/2;
+        long S2n=(n*(n+1)*(2*n+1))/6;
+        long S=0,S2=0;
         for(int i=0;i<n;i++){
-            hash[nums[i]]++;
+            S+=nums[i];
+            S2+=(long)nums[i] * (long)nums[i];
         }
-        for(int i=1;i<=n;i++){
-            if(hash[i]==2) repeating=i;
-            else if(hash[i]==0) missing=i;
-        }
-        int ans[]={repeating,missing};
+        long val1=S-Sn;//x-y
+        long val2=S2-S2n;//x+y
+        val2=val2/val1;
+        long x=(val1+val2)/2;
+        long y =x-val1;
+        int ans[]={(int)x,(int)y};
         return ans;
     }
     
