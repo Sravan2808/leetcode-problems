f870bd61552c686f9e6b79ab8f64fd93b3f81970
Sravan2808 <141347235+Sravan2808@users.noreply.github.com>
2024-09-22 19:50:12 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (16.97%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        // BruteForce TC:O(N^2),SC:O(1)
+        // int repeating=-1,missing=-1;
+        // for(int i=1;i<=nums.length;i++){
+        //     int cnt=0;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(nums[j]==i)
+        //         cnt++;
+        //     }
+        //     if(cnt==2) repeating=i;
+        //     else if(cnt==0) missing=i;
+        //     if(repeating!=-1 && missing!=-1) break;
+        // }
+        // int ans[] ={repeating,missing};
+        // return ans;
+
+        // BetterApproach(Hashing)  
         int repeating=-1,missing=-1;
-        for(int i=1;i<=nums.length;i++){
-            int cnt=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]==i)
-                cnt++;
-            }
-            if(cnt==2) repeating=i;
-            else if(cnt==0) missing=i;
-            if(repeating!=-1 && missing!=-1) break;
+        int n = nums.length;
+        int[] hash = new int[n+1];
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
         }
-        int ans[] ={repeating,missing};
+        for(int i=1;i<=n;i++){
+            if(hash[i]==2) repeating=i;
+            else if(hash[i]==0) missing=i;
+        }
+        int ans[]={repeating,missing};
         return ans;
-        
     }
     
 }
