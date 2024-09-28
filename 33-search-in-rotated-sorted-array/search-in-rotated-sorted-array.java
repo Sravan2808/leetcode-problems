72c526104ae26a074d1fee803075014681accc54
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-09-28 17:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (69.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,24 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low=0,high=nums.length-1;
+        // Bruteforce Approach TimeComplexity:O(n)
+        // int ans =-1;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] == target)
+        //         ans=i;
+        // }
+        // return ans;
+        
+        int n = nums.length;
+        int low = 0,high=n-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target)
-            return mid;
-
+            int mid = (low+high)/2;
+            if(nums[mid] == target) return mid;
+            // left Sorted
             if(nums[low]<=nums[mid]){
-                if(nums[low] <=target && target <= nums[mid])
+                if(nums[low]<=target && target<=nums[mid])
                 high=mid-1;
                 else
                 low=mid+1;
             }
+
+            // right Sorted
             else{
-                if(nums[mid] <= target && target <= nums[high])
-                low=mid+1;
-                else
-                high=mid-1;
+            if(nums[mid]<=target && target<=nums[high])
+            low=mid+1;
+            else
+            high=mid-1;
             }
-            
         }
         return -1;
     }
