092dbfe310e808fd0d9b033d090b01f37aea6491
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-10-05 10:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (93.37%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,40 +1,31 @@
 class Solution {
-   public static int countPartitions(int[] a, int maxSum) {
-        int n = a.length; //size of array.
-        int partitions = 1;
-        long subarraySum = 0;
-        for (int i = 0; i < n; i++) {
-            if (subarraySum + a[i] <= maxSum) {
-                //insert element to current subarray
-                subarraySum += a[i];
-            } else {
-                //insert element to next subarray
-                partitions++;
-                subarraySum = a[i];
-            }
+    int fn(int arr[],int mid){
+       int noOfPages=0,std=1;
+       for(int i=0;i<arr.length;i++)
+       {
+        if(noOfPages+arr[i]>mid){
+            std++;
+            noOfPages=arr[i];
         }
-        return partitions;
+        else noOfPages+=arr[i];
+       }
+       return std;
+        
     }
     public int splitArray(int[] nums, int k) {
-       int low = nums[0];
-        int high = 0;
-        //find maximum and summation:
-        for (int i = 0; i < nums.length; i++) {
-            low = Math.max(low, nums[i]);
-            high += nums[i];
-        }
-
-        //Apply binary search:
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int partitions = countPartitions(nums, mid);
-            if (partitions > k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return low;
+       int sum=0;
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+        max=Math.max(max,nums[i]);
+       }
+       int low=max,high=sum;
+       while(low<=high){
+        int mid=(low+high)/2;
+        if(fn(nums,mid)>k) low=mid+1;
+        else high=mid-1;
+       }
+       return low;
     }
 
 }
