4ca4107ad4b1442b3c0c99938503a2952e73dd08
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-10-08 22:53:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (11.70%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,14 +1,14 @@
 class Solution {
-    public boolean binarySearch(int nums[],int target){
-        int low = 0, high=nums.length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target) return true;
-            else if(target>nums[mid]) low=mid+1;
-            else high = mid-1;
-        }
-        return false;
-    }
+    // public boolean binarySearch(int nums[],int target){
+    //     int low = 0, high=nums.length-1;
+    //     while(low<=high){
+    //         int mid=(low+high)/2;
+    //         if(nums[mid]==target) return true;
+    //         else if(target>nums[mid]) low=mid+1;
+    //         else high = mid-1;
+    //     }
+    //     return false;
+    // }
     public boolean searchMatrix(int[][] matrix, int target) {
         // Brute Force Approach
         // for(int i=0;i<matrix.length;i++){
@@ -20,13 +20,27 @@ class Solution {
         // }
         // return false;
 
-        //Better Approach
-        int m=matrix[0].length;
-        for(int i =0;i<matrix.length;i++){
-            if(matrix[i][0] <= target && target<=matrix[i][m-1]){
-                return binarySearch(matrix[i],target);
-            }
+        // //Better Approach
+        // int n=matrix[0].length;
+        // for(int i =0;i<matrix.length;i++){
+        //     if(matrix[i][0] <= target && target<=matrix[i][n-1]){
+        //         return binarySearch(matrix[i],target);
+        //     }
+        // }
+        // return false;
+
+        // Optimize Approach TC:O(log(m*n)) SC:O(1)
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int low = 0,high = n*m-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int row = mid/m , col=mid%m;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
         }
         return false;
+
     }
 }
