028829633e0291a20974b579392221443b5cd0ef
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-10-14 19:20:34 +0530

    Time: 10 ms (26.69%) | Memory: 71.6 MB (5.86%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,46 +9,50 @@
  * }
  */
 class Solution {
-    // reversing the linklist
-    public ListNode reverseList(ListNode head){
-        ListNode prev=null;
-        ListNode next=null;
-        ListNode current=head;
-        while(current!=null){
-        next=current.next;
-        current.next=prev;
-        prev=current;
-        current=next;
-        }
-        head=prev;
-        return head;
+    public ListNode reverse(ListNode head){
+        if(head==null || head.next==null) return head;
+        ListNode newHead = reverse(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+
     }
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null)
-        return true;
-       
-        // finding the midddle element
-        ListNode slow=head;
-        ListNode fast=head;
+        // BruteForce Approach TC:O(2N) SC:O(N)
+        // Stack<Integer> st = new Stack<Integer>();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     st.push(temp.val);
+        //     temp=temp.next;
+        // }
+        // temp = head;
+        // while(temp!=null){
+        //     if(temp.val!=st.peek()) return false;
+        //     temp=temp.next;
+        //     st.pop();
+        // }
+        // return true;
+
+        // OptimalApproach TC:O(2N),SC:O(1)
+        ListNode slow = head;
+        ListNode fast = head;
         while(fast.next!=null && fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
-        ListNode newhead=reverseList(slow.next);
-        ListNode first=head;
-        ListNode second=newhead;
-       
-        while(second!=null && first!=null){
-            if(first.val!=second.val){ 
-             reverseList(newhead);
-            return false;
+        ListNode newHead = reverse(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+        while(second!=null){
+            if(first.val!=second.val){
+                reverse(newHead);
+                return false;
             }
-            
-                first=first.next;
-                second=second.next;
+            first = first.next;
+            second = second.next;
         }
-         reverseList(newhead);
+        reverse(newHead);
         return true;
-        
     }
 }
