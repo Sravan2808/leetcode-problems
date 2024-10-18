b4349449befa3e50751993c05313f125b2a5cdea
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-10-18 19:12:59 +0530

    Time: 1 ms (99.87%) | Memory: 47.5 MB (97.95%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -27,33 +27,45 @@ public class Solution {
 
         // BetterApproach TC:O(n1+2n2) SC:O(1)
 
-        ListNode t1 = headA;
-        ListNode t2 = headB;
-        int N1=0,N2=0;
-        while(t1!=null){
-            N1++;
-            t1=t1.next;
-        }
-        while(t2!=null){
-            N2++;
-            t2=t2.next;
-        }
-        if(N1<N2)
-            return collisionPart(headA,headB,N2-N1);
-        else
-        return collisionPart(headB,headA,N1-N2);
+    //     ListNode t1 = headA;
+    //     ListNode t2 = headB;
+    //     int N1=0,N2=0;
+    //     while(t1!=null){
+    //         N1++;
+    //         t1=t1.next;
+    //     }
+    //     while(t2!=null){
+    //         N2++;
+    //         t2=t2.next;
+    //     }
+    //     if(N1<N2)
+    //         return collisionPart(headA,headB,N2-N1);
+    //     else
+    //     return collisionPart(headB,headA,N1-N2);
+    // }
+    // ListNode collisionPart(ListNode t1,ListNode t2,int d){
+    //     while(d!=0){
+    //         d--;
+    //         t2=t2.next;
+    //     }
+    //     while(t1!=t2){
+    //         t1=t1.next;
+    //         t2=t2.next;
+    //     }
+    //     return t1;
+
+    // Optimal Approach
+    if(headA==null || headB==null) return null;
+    ListNode t1 = headA , t2 = headB;
+    while(t1!=t2){
+        t1=t1.next;
+        t2=t2.next;
+        if(t1==t2) return t1;
+        if(t1==null) t1=headB;
+        if(t2==null) t2=headA;
     }
-    ListNode collisionPart(ListNode t1,ListNode t2,int d){
-        while(d!=0){
-            d--;
-            t2=t2.next;
-        }
-        while(t1!=t2){
-            t1=t1.next;
-            t2=t2.next;
-        }
-        return t1;
-        
+    return t1;
+    
     }
 
 }
