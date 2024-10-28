a951602f830786b2a96e9847fe6f5e3e7c1be50d
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-10-28 18:20:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (13.01%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,11 @@
 class Solution {
-    public double ans(double x,long n){
-        // 1.Base case condition
-        if(n==0)
-        return 1;
-        // n is negative
-        if(n<0)
-        return ans(1/x,-n);
-        // if n is even
-        if(n%2==0){
-            return ans(x*x,n/2);
-        }
-        else
-        return x*ans(x*x,(n-1)/2);
+    double solve(double x , long n){
+        if(n == 0) return 1;
+        if(n<0) return solve(1/x,-n);
+        if(n%2==0) return solve(x*x,n/2);
+        else return x*solve(x*x,(n-1)/2);
     }
     public double myPow(double x, int n) {
-                return ans(x,(long)n);
-                        
+     return solve(x,(long)n);   
     }
 }
