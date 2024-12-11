319604dcc44fac09e83ce28f3e4867bc3db2a70a
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-12-11 20:45:00 +0530

    Time: 38 ms (77.47%) | Memory: 60.1 MB (61.39%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,33 +1,16 @@
 class Solution {
-    public int binarySearch(int nums[],int target){
-        int result = 0;
-        int l=0;
-        int r=nums.length-1;
-
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]<=target){
-                result=mid;
-                l=mid+1;
-            }else{
-                r=mid-1;
-            }
-        }
-        return result;
-    }
     public int maximumBeauty(int[] nums, int k) {
         int n = nums.length;
         Arrays.sort(nums);
-
+        int i=0,j=0;
         int maxBeauty = 0;
-        for(int i=0;i<n;i++){
-            int x = nums[i];
-            int y = x+2*k;
-            // find the farthest y<=x+2k
-            int j = binarySearch(nums,y);
-            maxBeauty = Math.max(maxBeauty,j-i+1);
+        while(i<n){
+            while(j<n && nums[j]<=nums[i]+2*k){
+                j++;
+            }
+            maxBeauty = Math.max(maxBeauty,j-i);
+            i++;
         }
-      return maxBeauty;
-
+        return maxBeauty;
     }
 }
