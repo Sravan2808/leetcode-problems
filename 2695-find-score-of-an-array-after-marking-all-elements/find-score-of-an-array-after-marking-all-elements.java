8263bca4a1c8bbf7a4935f46dfc95bdf8ea7db2a
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-12-13 19:15:33 +0530

    Time: 113 ms (67.65%) | Memory: 59.7 MB (51.39%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,21 +1,22 @@
 class Solution {
     public long findScore(int[] nums) {
-        int n = nums.length;
-        boolean visited[] = new boolean[n];
-        PriorityQueue<int[] > pq = new PriorityQueue<>((a,b) -> {
-            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
-            return Integer.compare(a[1],b[1]);
-        });
+       int n = nums.length;
+       long score = 0;
 
-        for(int i=0;i<n;i++){
-            pq.add(new int[]{nums[i],i});
-        }
-        long score = 0;
-        while(!pq.isEmpty()){
-            int[] element = pq.poll();
-        
-        int value = element[0];
-        int index = element[1];
+       int pairs[][] = new int[n][2];
+       for(int i=0;i<n;i++){
+        pairs[i][0] = nums[i];
+        pairs[i][1] = i;
+       }
+       Arrays.sort(pairs,(a,b) -> {
+        if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
+        return Integer.compare(a[1],b[1]);
+       });
+       boolean visited[] = new boolean[n];
+
+       for(int i=0;i<n;i++){
+        int value = pairs[i][0];
+        int index = pairs[i][1];
 
         if(!visited[index]){
             score+=value;
@@ -24,11 +25,11 @@ class Solution {
             if(index-1>=0){
                 visited[index-1] = true;
             }
-            if(index+1 < n){
+            if(index+1<n){
                 visited[index+1] = true;
             }
         }
-        }
-        return score;
+       }
+       return score;
     }
 }
