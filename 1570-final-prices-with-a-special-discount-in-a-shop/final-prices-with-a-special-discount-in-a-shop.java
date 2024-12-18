fd18132e5f11818901d0c408b84959554be4c8da
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2024-12-18 11:02:44 +0530

    Time: 4 ms (40.47%) | Memory: 43.9 MB (38.43%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -1,16 +1,27 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         // bruteForce Approach
+        // int n = prices.length;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(prices[j]<=prices[i]){
+        //             prices[i]-=prices[j];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return prices;
+        
+        // Monotonic Stack
+        Stack<Integer> st = new Stack<>();
         int n = prices.length;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(prices[j]<=prices[i]){
-                    prices[i]-=prices[j];
-                    break;
-                }
+            while(!st.isEmpty() && prices[st.peek()] >= prices[i]){
+            prices[st.peek()] -= prices[i];
+            st.pop();
             }
+            st.push(i);
         }
         return prices;
-        
     }
 }
