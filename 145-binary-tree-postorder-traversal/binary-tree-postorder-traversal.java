449dfacf40b178c64ccf9c83bbedbffe8855c7f9
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2025-01-03 18:56:57 +0530

    Time: 1 ms (8.37%) | Memory: 41.8 MB (21.17%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,17 +14,24 @@
  * }
  */
 class Solution {
-    public static void postOrder(TreeNode root , List<Integer> result){
-        if(root == null) return;
-        postOrder(root.left,result);
-        postOrder(root.right,result);
-        result.add(root.val);
-    }
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        postOrder(root,result);
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        ArrayList<Integer> postOrder = new ArrayList<>();
 
-        return result;
-        
+        if(root == null) return postOrder;
+
+        st1.push(root);
+        while(!st1.isEmpty()){
+             root = st1.pop();
+             st2.push(root);
+
+             if(root.left!=null) st1.push(root.left);
+             if(root.right!=null) st1.push(root.right);
+        }
+        while(!st2.isEmpty())
+        postOrder.add(st2.pop().val);
+        return postOrder;
     }
+
 }
