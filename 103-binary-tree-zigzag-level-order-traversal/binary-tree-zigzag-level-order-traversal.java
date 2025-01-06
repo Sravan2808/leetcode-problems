ceef29238aa7b1672b417b4b17745d361eada5a1
SRAVAN SURYA <141347235+Sravan2808@users.noreply.github.com>
2025-01-06 20:39:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (64.16%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,31 +15,42 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result=new ArrayList<>();
-        if(root==null)
-        return result;
-        Queue<TreeNode> q=new LinkedList<>();
+        // Creating an array list to result the elements at the end
+        List<List<Integer>> result = new ArrayList<>();
 
+        // BaseCase Condition if the root is ending stage
+        if(root == null) return result;
+
+        // Creating a Queue to store the elements
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        int lvl=0;
+        // LevelOrder
+        int lvl = 0;
+        // Traversal Queue ,Until it is Empty
         while(!q.isEmpty()){
-            List<Integer> level=new ArrayList<>();
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                TreeNode x=q.remove();
-                level.add(x.val);
-                if(x.left!=null)
-                    q.add(x.left);
-                if(x.right!=null)
-                    q.add(x.right);
-            }
-            if(lvl%2==1)//odd
-                Collections.reverse(level);
-                result.add(level);
-                lvl++;
+
+        // Create a Level ArrayList to Store the elements
+        List<Integer> level = new ArrayList<>();
+        int size = q.size();
+        for(int i=0;i<size;i++){
+            TreeNode x = q.remove();
+            level.add(x.val);
+            if(x.left!=null) q.add(x.left);
+            if(x.right!=null) q.add(x.right);
+        }
+
+        // Checking the level is even or off
+
+            if(lvl %2 == 1) //It is Odd
+            // So we should reverse the array
+            Collections.reverse(level);
+
+            // Add Up to the result;
+            result.add(level);
+            lvl++;
         }
         return result;
-        
+
         
     }
 }
