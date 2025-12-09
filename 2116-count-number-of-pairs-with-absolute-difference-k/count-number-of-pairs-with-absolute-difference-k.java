3b4cb7037ea60d8503d938b8af5e7a14b826c8dc
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2025-12-09 08:42:13 +0530

    Time: 6 ms (55.01%) | Memory: 46 MB (13.54%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -1,23 +1,36 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-            else{
-            map.put(nums[i],1);
-            }
-        }
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.containsKey(nums[i])){
+        //         map.put(nums[i],map.get(nums[i])+1);
+        //     }
+        //     else{
+        //     map.put(nums[i],1);
+        //     }
+        // }
+
+        // int cnt = 0;
+        // for(int num : nums){
+        //     int com = k+num;
+        //     if(map.containsKey(com)){
+        //         cnt+=map.get(com);
+        //     }
+        // }
+        // return cnt;
 
         int cnt = 0;
-        for(int num : nums){
-            int com = k+num;
-            if(map.containsKey(com)){
-                cnt+=map.get(com);
-            }
+
+       for(int i=0;i<nums.length;i++){
+        if(map.containsKey(k+nums[i])){
+            cnt += map.get(k+nums[i]);
         }
-        return cnt;
-        
+        if(map.containsKey(nums[i]-k)){
+            cnt += map.get(nums[i]-k);
+        }
+
+       map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+       }
+       return cnt;
     }
 }
