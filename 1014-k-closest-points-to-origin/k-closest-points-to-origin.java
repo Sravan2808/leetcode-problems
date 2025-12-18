feba687e6398df75046fb1a587e3ae4ddce68aef
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2025-12-18 12:51:23 +0530

    Time: 21 ms (91.07%) | Memory: 60.9 MB (5.11%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,38 +1,8 @@
 class Solution {
-    public class Point implements Comparable<Point>{
-            int x ;
-            int y;
-            double distance;
-            int i;
-
-            // constructor initalization
-            public Point(int x,int y,double distance ,int i){
-                this.x = x;
-                this.y = y;
-                this.distance = distance;
-                this.i = i;
-            }
-            @Override
-            public int compareTo(Point p1){
-                // sort the data on the basics of distance value
-                // in Ascending Order
-                return Double.compare(this.distance,p1.distance);
-            }
-        }
     public int[][] kClosest(int[][] points, int k) {
-        //1.Build of minheap
-        PriorityQueue<Point> minHeap =  new PriorityQueue<>();
-        for(int i=0;i<points.length;i++){
-            // Euclidean distance formula
-            double distance = Math.sqrt(points[i][0] * points[i][0] + points[i][1]*points[i][1]);
-            minHeap.add(new Point(points[i][0],points[i][1],distance,i));
-        }
-        //2.Deletion until k number of times
-        int result[][] = new int[k][];
-        for(int i=0;i<k;i++){
-            Point point = minHeap.remove();
-            result[i] = new int[]{points[point.i][0],points[point.i][1]};
-        }
-        return result;
+        Arrays.sort(points, (a, b) -> {
+        return ((a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1]));
+        });
+        return Arrays.copyOfRange(points, 0, k);
     }
 }
