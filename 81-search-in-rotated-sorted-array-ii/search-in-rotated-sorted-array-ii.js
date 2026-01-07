09d4022925904a1bd99a0f5d4fc8fc33966d42e4
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-01-08 01:03:53 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (47.85%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+    
+    let l = 0,r=nums.length-1;
+    while(l<=r){
+        let mid = Math.floor(l+(r-l)/2);
+        if(nums[mid]==target) return true;
+
+        if(nums[l]==nums[mid] && nums[mid]==nums[r]){
+            l++;
+            r--;
+            continue;
+        }
+
+        // left sorted array
+        if(nums[l]<=nums[mid]){
+            if(nums[l]<=target && target<=nums[mid]) r=mid-1;
+            else l=mid+1;
+        }
+        else{
+            if(nums[mid]<=target && target<=nums[r]) l=mid+1;
+            else r=mid-1;
+        }
+    }
+    return false;
+};
