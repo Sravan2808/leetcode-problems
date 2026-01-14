30735f21e496f4b0bdce27c7b9109a5ac70f8d5d
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-01-15 00:39:09 +0530

    Time: 10 ms (52.84%) | Memory: 59.6 MB (30.95%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,22 +9,51 @@
  * }
  */
 class Solution {
+    ListNode findMiddle(ListNode head){
+        ListNode slow=head,fast=head;
+        ListNode prev = null;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return prev;
+    }
+
+    public ListNode merge(ListNode left,ListNode right){
+        ListNode curr1 = left;
+        ListNode curr2 = right;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr3 = dummy;
+
+        while(curr1!=null && curr2!=null){
+            if(curr1.val<=curr2.val){
+                curr3.next=curr1;
+                curr1=curr1.next;
+            }
+            else{
+            curr3.next=curr2;
+            curr2=curr2.next;
+            }
+
+            curr3=curr3.next;
+        }
+        if(curr1!=null) curr3.next=curr1;
+        if(curr2!=null) curr3.next=curr2;
+
+        return dummy.next;
+    }
     public ListNode sortList(ListNode head) {
-        List<Integer> arr = new ArrayList<Integer>();
-        ListNode temp = head;
-        while(temp!=null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
-        Collections.sort(arr);
-        int i=0;
-        temp = head;
-        while(temp!=null){
-            temp.val=arr.get(i);
-            i++;
-            temp=temp.next;
-        }
-        return head;
-        
+        if(head==null || head.next==null) return head;
+
+        ListNode middleNode = findMiddle(head);
+        ListNode head2 = middleNode.next;
+        middleNode.next=null;
+
+        ListNode left = sortList(head); 
+        ListNode right = sortList(head2);
+        ListNode merged = merge(left,right);
+        return merged;
     }
 }
