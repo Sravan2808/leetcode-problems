725445d7bade5482dfc790b086082a8ee14fd925
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-01-19 00:47:55 +0530

    Time: 3 ms (99.69%) | Memory: 93.9 MB (96.61%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,50 +9,37 @@
  * }
  */
 class Solution {
-    public ListNode reverse(ListNode head){
-        if(head==null || head.next==null) return head;
-        ListNode newHead = reverse(head.next);
-        ListNode front = head.next;
-        front.next = head;
-        head.next = null;
-        return newHead;
-
-    }
     public boolean isPalindrome(ListNode head) {
-        // BruteForce Approach TC:O(2N) SC:O(N)
-        // Stack<Integer> st = new Stack<Integer>();
-        // ListNode temp = head;
-        // while(temp!=null){
-        //     st.push(temp.val);
-        //     temp=temp.next;
-        // }
-        // temp = head;
-        // while(temp!=null){
-        //     if(temp.val!=st.peek()) return false;
-        //     temp=temp.next;
-        //     st.pop();
-        // }
-        // return true;
-
-        // OptimalApproach TC:O(2N),SC:O(1)
         ListNode slow = head;
-        ListNode fast = head;
-        while(fast.next!=null && fast.next.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        ListNode fast = head.next;
+
+        if(head==null || head.next==null) return true;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ListNode newHead = reverse(slow.next);
-        ListNode first = head;
-        ListNode second = newHead;
-        while(second!=null){
-            if(first.val!=second.val){
-                reverse(newHead);
-                return false;
-            }
-            first = first.next;
-            second = second.next;
+
+        ListNode nextPointer = slow.next;
+        slow.next = null;
+
+        ListNode prev = null;
+        while(nextPointer!=null){
+            ListNode temp = nextPointer.next;
+            nextPointer.next = prev;
+            prev = nextPointer;
+            nextPointer = temp;
         }
-        reverse(newHead);
+
+        ListNode right = prev; 
+        ListNode left = head;
+
+        while(right!=null){
+            if(left.val != right.val) return false;
+            left=left.next;
+            right=right.next;
+        }
         return true;
+
     }
 }
