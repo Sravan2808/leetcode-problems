729058029bef6a96987c5e504a6155075ccb0217
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-02-27 21:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (86.05%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,12 +14,28 @@
  * }
  */
 class Solution {
-    List<Integer> result = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root==null) return result;
-        inorderTraversal(root.left);
-        result.add(root.val);
-        inorderTraversal(root.right);
-        return result;
+        List<Integer> inorder = new ArrayList<Integer>();
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left==null){
+                inorder.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode temp = curr.left;
+                while(temp.right!=null && temp.right!=curr) temp = temp.right;
+                if(temp.right==null){
+                    temp.right = curr;
+                    curr=curr.left;
+                }else{
+                    temp.right=null;
+                    inorder.add(curr.val);
+                    curr=curr.right;
+                }
+            }
+        }
+        return inorder;
+        
     }
 }
