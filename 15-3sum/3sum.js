10a42fce948e9ecbf32febd0d6e1670578a35399
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-03-10 11:52:48 +0530

    Time: 35 ms (66.34%) | Memory: 73.3 MB (25.25%) - LeetSync

---
+++ 15-3sum/3sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let i=0;
+    let res = [];
+    nums.sort((a,b)=>a-b)
+    for(let i=0;i<nums.length-2;i++){
+        if(i!=0 && nums[i]==nums[i-1]) continue;
+        let j=i+1,k=nums.length-1;
+        while(j<k){
+            let sum = nums[i]+nums[j]+nums[k];
+            if(sum == 0){
+                res.push([nums[i],nums[j],nums[k]]);
+                j++;
+                k--;
+                while(j<k && nums[j]==nums[j-1]) j++;
+                while(j<k && nums[k+1]==nums[k]) k--;
+            }
+            else if(sum<0) j++;
+            else k--;
+        }
+    }
+    return res;
+};
