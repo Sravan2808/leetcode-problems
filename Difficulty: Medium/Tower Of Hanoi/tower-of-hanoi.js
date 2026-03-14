43440da1c5eb48e1095a6cbb8a309191e6cbe2d4
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-03-15 00:36:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} from
+ * @param {number} to
+ * @param {number} aux
+ * @returns {number}
+ */
+class Solution {
+    towerOfHanoi(n, from, to, aux) {
+        // code here
+                return (Math.pow(2,n)-1);
+
+    }
+}
