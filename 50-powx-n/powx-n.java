f2d20674c28bf599e4770f2096bf0b15bdab517a
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-07-27 16:38:29 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (34.75%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -2,22 +2,34 @@ class Solution {
     public double myPow(double x, int n) {
 
         // Iterative Approach Time Complexity:O(logn) Space Complexity:O(1)
-        double ans = 1.0;
-        long power = n;
-        if(power<0){
-            x=1/x;
-            power=-1*power;
+        // double ans = 1.0;
+        // long power = n;
+        // if(power<0){
+        //     x=1/x;
+        //     power=-1*power;
+        // }
+        // while(power>0){
+        //     if(power%2==0){
+        //         x=x*x;
+        //         power=power/2;
+        //     }else{
+        //         ans=ans*x;
+        //         power=power-1;
+        //     }
+        // }
+        // return ans;
+
+        // Recursive Approach : Time Complexity:O(logn) Space Complexity:O(logn)
+        long num = n;
+        if(num<0){
+            return 1.0/recursiveApproach(x,-1*num);
         }
-        while(power>0){
-            if(power%2==0){
-                x=x*x;
-                power=power/2;
-            }else{
-                ans=ans*x;
-                power=power-1;
-            }
-        }
-        return ans;
+        return recursiveApproach(x,num);
+    }
+    private double recursiveApproach(double x,long n){
+        if(n==0) return 1;
         
+        if(n%2!=0) return x*recursiveApproach(x,n-1);
+        return recursiveApproach(x*x,n/2);
     }
 }
