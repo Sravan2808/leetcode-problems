ec736f30fd4fb6b8aaf3e3ef2db42986861c8dcb
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-07-28 11:40:47 +0530

    Time: 32 ms (53.82%) | Memory: 48.1 MB (57.43%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,31 +1,39 @@
 class Solution {
     public String smallestPalindrome(String s) {
-        Map<Character,Integer> map = new TreeMap<>();
-        for(char ch:s.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
+        // Map<Character,Integer> map = new TreeMap<>();
+        // for(char ch:s.toCharArray()){
+        //     map.put(ch,map.getOrDefault(ch,0)+1);
+        // }
 
-        StringBuilder str = new StringBuilder();
-        char mid = ' ';
-        boolean found = false;
-        for(char c:map.keySet()){
-            int freq = map.get(c);
-            for(int i=0;i<freq/2;i++){
-                str.append(c);
-            }
-            if(freq%2!=0){
-                mid=c;
-                found = true;
-            }
-        }
+        // StringBuilder str = new StringBuilder();
+        // char mid = ' ';
+        // boolean found = false;
+        // for(char c:map.keySet()){
+        //     int freq = map.get(c);
+        //     for(int i=0;i<freq/2;i++){
+        //         str.append(c);
+        //     }
+        //     if(freq%2!=0){
+        //         mid=c;
+        //         found = true;
+        //     }
+        // }
 
-        String reversed = new StringBuilder(str).reverse().toString();
-        
-        if(found) str.append(mid);
+        // String reversed = new StringBuilder(str).reverse().toString();
 
-        str.append(reversed);
+        // if(found) str.append(mid);
 
-        return str.toString();
-        
+        // str.append(reversed);
+
+        // return str.toString();
+
+        char half[] = s.substring(0, s.length() / 2).toCharArray();
+        Arrays.sort(half);
+
+        String res = new String(half) + ((s.length() & 1) == 1 ? s.charAt(s.length() / 2) : "") +
+                new StringBuilder(new String(half)).reverse();
+
+        return res;
+
     }
 }
