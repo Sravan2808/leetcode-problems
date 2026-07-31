bcbf11b675970a886989e1b966a4dde4b20520e9
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-07-31 16:26:23 +0530

    Time: 12 ms (7.66%) | Memory: 46.5 MB (65.25%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,58 +1,62 @@
 class Solution {
-    void queen(char[][] board,int row,List<List<String>> ans){
-        if(row == board.length){
-            ans.add(makeString(board));
-            return;
+    private boolean isSafe(List<String> board, int row, int col) {
+        int r = row, c = col;
+        // upperRight
+        while (r >= 0 && c < board.get(0).length()) {
+            if (board.get(r).charAt(c) == 'Q')
+                return false;
+            r--;
+            c++;
         }
-        for(int col = 0;col<board.length;col++){
-            if(isSafe(board,row,col)){
-                board[row][col]='Q';
-                queen(board,row+1,ans);
-                board[row][col] = '.';
-            }
-        }
-    }
-    public List<List<String>> solveNQueens(int n) {
-        char[][] board = new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                board[i][j] = '.';
-            }
-        }
-        List<List<String>> ans = new ArrayList<>();
-        queen(board,0,ans);
-        return ans;
-    }
-    public boolean isSafe(char[][] board,int row,int col){
-        // for checking vertical row
-        for(int i=0;i<row;i++){
-            if(board[i][col] == 'Q')
-            return false;
-        }
-    
 
-     // for checking left diagonal
-        int maxLeft = Math.min(row,col);
-        for(int i=1;i<=maxLeft;i++){
-            if(board[row-i][col-i] == 'Q')
-            return false;
+        r = row;
+        c = col;
+        // upper Left'
+        while(r>=0 && c>=0){
+            if(board.get(r).charAt(c)=='Q') return false;
+            r--;
+            c--;
         }
-    
-    // for checking right diagonal
-    int maxRight = Math.min(row,board.length-1-col);
-     for(int i=1;i<=maxRight;i++){
-            if(board[row-i][col+i] == 'Q')
-            return false;
+
+        r=row;
+        c=col;
+
+        // Top
+        while(r>=0){
+            if(board.get(r).charAt(c)=='Q') return false;
+            r--;
         }
+
         return true;
     }
-     List<String> makeString(char[][] board){
-        List<String> L = new ArrayList<>();
-        for(int i=0;i<board.length;i++){
-            String row = new String(board[i]);
-            L.add(row);
+
+    private void func(int row, List<List<String>> ans, List<String> board) {
+        if (row == board.size()) {
+            ans.add(new ArrayList<>(board));
+            return;
         }
-        return L;
+        for (int col = 0; col < board.get(0).length(); col++) {
+            if (isSafe(board, row, col)) {
+                char rowArr[] = board.get(row).toCharArray();
+                rowArr[col] = 'Q';
+                board.set(row, new String(rowArr));
+                func(row + 1, ans, board);
+                rowArr[col] = '.';
+                board.set(row, new String(rowArr));
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> board = new ArrayList<>();
+
+        // Making row wise column "."
+        for (int row = 0; row < n; row++) {
+            board.add(".".repeat(n));
+        }
+
+        func(0, ans, board);
+        return ans;
     }
 }
-
