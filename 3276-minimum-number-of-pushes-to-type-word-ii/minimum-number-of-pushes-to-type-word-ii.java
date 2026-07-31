11b2a0ff55d68a986ed86e04993561bd32db9d33
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-07-31 11:59:34 +0530

    Time: 18 ms (23.41%) | Memory: 48 MB (73.70%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     public int minimumPushes(String word) {
-        int map[] = new int[26];
+        Integer map[] = new Integer[26];
+        Arrays.fill(map,0);
         for (char ch : word.toCharArray()) {
             map[ch - 'a']++;
         }
 
-        // Convert int to Integer Object we use Streams;
-        Integer mapInteger[] = Arrays.stream(map).boxed().toArray(Integer[]::new);
 
         // Convert this descending order according to the frequency
-        Arrays.sort(mapInteger, (a,b)->b-a);
+        Arrays.sort(map, (a,b)->b-a);
 
         int ans = 0;
         for (int i = 0; i < 26; i++) {
-            int freq = mapInteger[i];
+            int freq = map[i];
             ans += freq * (i / 8 + 1);
         }
         return ans;
