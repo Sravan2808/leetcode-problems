c0a592f7f2b9658ea80b9b4fc4bb60da71df47a5
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-07-31 14:42:33 +0530

    Time: 130 ms (73.31%) | Memory: 42.8 MB (76.21%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,27 +1,28 @@
 class Solution {
-    boolean solve(char[][] board, String word, int i, int j, int cnt) {
-        if (cnt == word.length())
-            return true;
-        if (i == -1 || j == -1 || i == board.length || j == board[0].length || board[i][j] != word.charAt(cnt))
-            return false;
-        char temp = board[i][j];
-        board[i][j] = '.';
-        boolean isFound = solve(board, word, i, j - 1, cnt + 1) || solve(board, word, i, j + 1, cnt + 1)
-                || solve(board, word, i + 1, j, cnt + 1) || solve(board, word, i - 1, j, cnt + 1);
+    private boolean func(int i,int j,int index,char[][] board,String word){
+        if(index==word.length()) return true;
 
-        board[i][j] = temp;
-        return isFound;
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] ==' ' || board[i][j]!=word.charAt(index) ) return false;
+
+        char ch = board[i][j];
+        board[i][j] = ' ';
+
+        // Traverse Up Down Left Right
+        boolean ans = func(i-1,j,index+1,board,word) || func(i+1,j,index+1,board,word) || func(i,j-1,index+1,board,word) || func(i,j+1,index+1,board,word);
+
+        board[i][j] = ch;
+        return ans;
     }
 
     public boolean exist(char[][] board, String word) {
-
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if ((board[i][j] == word.charAt(0)) && solve(board, word, i, j, 0))
-                return true;
+                if (board[i][j] == word.charAt(0)) {
+                    if (func(i, j, 0, board, word))
+                        return true;
+                }
             }
         }
         return false;
-
     }
 }
