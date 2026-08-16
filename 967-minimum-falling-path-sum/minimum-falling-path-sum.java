43c7d68dac351d7cda29b332cc8f55f74971f7d2
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-08-16 18:08:25 +0530

    Time: 7 ms (49.52%) | Memory: 46.9 MB (88.29%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -3,25 +3,28 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int dp[][] = new int[m][n];
+        // int dp[][] = new int[m][n];
+        int prev[] = new int[n];
 
         for(int j=0;j<n;j++){
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
 
         for(int i=1;i<m;i++){
+            int curr[] = new int[n];
             for(int j=0;j<n;j++){
-                int up = matrix[i][j] + dp[i-1][j];
+                int up = matrix[i][j] + prev[j];
                 int leftdiagonal =(int) 1e9;
-                if(j-1>=0) leftdiagonal=matrix[i][j]+dp[i-1][j-1];
+                if(j-1>=0) leftdiagonal=matrix[i][j]+prev[j-1];
                 int rightdiagonal = (int) 1e9;
-                if(j+1<n) rightdiagonal=matrix[i][j]+dp[i-1][j+1];
-                dp[i][j]=Math.min(up,Math.min(leftdiagonal,rightdiagonal));
+                if(j+1<n) rightdiagonal=matrix[i][j]+prev[j+1];
+                curr[j]=Math.min(up,Math.min(leftdiagonal,rightdiagonal));
             }
+            prev=curr;
         }
         int ans = Integer.MAX_VALUE;
         for(int j=0;j<n;j++){
-            ans = Math.min(ans,dp[m-1][j]);
+            ans = Math.min(ans,prev[j]);
         }
         return ans;
     }
